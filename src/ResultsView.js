630ddf2e008b1591bc2b75fcaea7f5380d128be3
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-07-06 16:52:53 +1000

    Better results page

--- src/ResultsView.js
+++ src/ResultsView.js
@@ -1,30 +1,78 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
-export default function ResultsView({result}) {
+// These should not be made into a pass or fail, nor should they be coloured
+const METADATA_LIST = ["UUID", "@timestamp", "platform", "version"];
+
+export default function ResultsView({ route, navigation }) {
+  const data = route.params.result;
+  let flatListKvPairs = [];
+
+  // Convert into a flatlist renderable format
+  for (const property in data) {
+    flatListKvPairs.push({ key: property, value: data[property] });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Here are your results</Text>
-      <Text>
-        {result}
-      </Text>
+      <Text style={styles.title}>Here are your results</Text>
+      <FlatList
+        data={flatListKvPairs}
+        renderItem={({ item, index }) => <Item key={index} item={item} />}
+      />
     </View>
   );
 }
 
-const Item = ({item}) => (
-  <View>
-    <Text>
-      {item.key}: {item.value}
-    </Text>
+// An individual item of the flatlist
+const Item = ({ item }) => {
+  let value;
+  let conditionalStyle = styles.item;
+  if (!METADATA_LIST.includes(item.key)) {
+    // Only the "tested" items (e.g. location services, pin/fingerprint)
+    value = item.value === 1 ? "Pass" : "Fail";
+    conditionalStyle = item.value === 1 ? itemSuccess : itemFail;
+  } else {
+    // Only the metadata items (e.g. UUID, @timestamp)
+    value = item.value;
+  }
+  return (
+    <View style={conditionalStyle}>
+      <Text style={styles.itemText}>
+        {item.key}: {value}
+      </Text>
     </View>
-)
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: "4%",
+  },
+  item: {
+    borderColor: "grey",
+    borderBottomWidth: 2,
+  },
+  success: {
+    backgroundColor: "#00FF00",
+  },
+  fail: {
+    backgroundColor: "red",
+  },
+  itemText: {
+    height: 44,
+    fontSize: 15,
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: "4%",
   },
 });
+
+const itemSuccess = StyleSheet.compose(styles.item, styles.success);
+const itemFail = StyleSheet.compose(styles.item, styles.fail);
--- src/ScanView.js
+++ src/ScanView.js
@@ -1,17 +1,62 @@
-import axios from 'axios';
-import JailMonkey from 'jail-monkey';
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import DeviceInfo from 'react-native-device-info';
-import {ES_INDEX_PATH, ES_PASSWORD, ES_URL, ES_USERNAME} from 'react-native-dotenv';
+import axios from "axios";
+import JailMonkey from "jail-monkey";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import DeviceInfo from "react-native-device-info";
+import {
+  ES_INDEX_PATH,
+  ES_PASSWORD,
+  ES_URL,
+  ES_USERNAME,
+} from "react-native-dotenv";
+import AsyncStorage from "@react-native-community/async-storage";
 
-const axiosInstance = axios.create({withCredentials: true, baseURL: ES_URL})
+const axiosInstance = axios.create({ withCredentials: true, baseURL: ES_URL });
 
-export default ScanView = ({navigation}) => {
+// Key for async storage last scan time, so user cannot spam scans
+const LAST_SCAN_TIME_KEY = "@last_scan_time";
+
+export default ScanView = ({ navigation }) => {
+  // split up onScan()
+  const sendToEs = async () => {
+    return;
+  };
 
   const onScan = async () => {
-    const timeOfScanUTC = new Date().toUTCString();
-    const boolPinOrFinger = await DeviceInfo.isPinOrFingerprintSet()
+    const timeOfScan = new Date();
+    // First check if we can scan
+    try {
+      // Stored in UTC
+      const lastScan = await AsyncStorage.getItem(LAST_SCAN_TIME_KEY);
+      // Will convert UTC String to local time
+      const lastScanDate = new Date(lastScan);
+      console.log("Last scan date in local time");
+      console.log(lastScanDate);
+
+      // number of milliseconds since beginning of time
+      let aDayLater = new Date(lastScan);
+      aDayLater.setDate(aDayLater.getDate() + 1);
+      console.log(aDayLater);
+
+      let canScan = aDayLater < timeOfScan ? true : false;
+      if (!canScan) {
+        console.log("Tell the user they must wait.");
+        const milliToWait = aDayLater - timeOfScan;
+        const hoursToWait = (milliToWait / (1000 * 60 * 60)).toFixed(1);
+        console.log(
+          `You must wait ${hoursToWait} hours until you can scan again`
+        );
+        // Don't continue
+        return;
+      }
+    } catch {
+      console.error("There was a problem reading from asyncStorage");
+      return;
+    }
+
+    // store as UTC for consistency with other indexed logs
+    const timeOfScanUTC = timeOfScan.toUTCString();
+    const boolPinOrFinger = await DeviceInfo.isPinOrFingerprintSet();
     const intPinOrFinger = boolPinOrFinger ? 1 : 0;
 
     const myDevice = DeviceInfo.getUniqueId();
@@ -28,61 +73,66 @@ export default ScanView = ({navigation}) => {
     const isNotJailBrokenInt = isJailBroken ? 0 : 1;
 
     const result = {
-      'UUID': myDevice,
-      '@timestamp': timeOfScanUTC,
-      'pinOrFingerPrintPass': intPinOrFinger,
-      'locationServicesPass': locServicesPassInt,
-      'platform': platform,
-      'version': version,
-      'isNotJailBroken': isNotJailBrokenInt,
+      UUID: myDevice,
+      "@timestamp": timeOfScanUTC,
+      pinOrFingerPrintPass: intPinOrFinger,
+      locationServicesPass: locServicesPassInt,
+      platform: platform,
+      version: version,
+      isNotJailBroken: isNotJailBrokenInt,
     };
 
     const resultJSON = JSON.stringify(result);
 
-    const config =
-        {
-          auth: {
-            username: ES_USERNAME,
-            password: ES_PASSWORD,
-          },
-          headers: {'Content-Type': 'application/json'}
-        }
-      
-    axiosInstance.post(ES_INDEX_PATH, resultJSON, config)
-        .then((res) => {
-          console.log('Result from elastic search')
-          console.log(res)
-          console.log('res status: ' + res.status)
-          if (res.status === 201) {
-            // Show the results of their privacy settings.
-                  // show the results page
-          navigation.push('Results', {
-            result: result
-          })
-          }
-          else {
-            console.error(
-                'Something went wrong sending the data to the dashboard')
-          }
-        })
-        .catch(
-            (err) => {
-                console.log('Error sending data to elasticsearch ', err)})
+    const config = {
+      auth: {
+        username: ES_USERNAME,
+        password: ES_PASSWORD,
+      },
+      headers: { "Content-Type": "application/json" },
+    };
 
-  }
+    axiosInstance
+      .post(ES_INDEX_PATH, resultJSON, config)
+      .then(async (res) => {
+        console.log("Result from elastic search");
+        console.log(res);
+        console.log("res status: " + res.status);
+        if (res.status === 201) {
+          // If all is successful, update the last scan time
+          await AsyncStorage.setItem(LAST_SCAN_TIME_KEY, timeOfScanUTC);
+
+          // Show the results of their privacy settings.
+          // show the results page
+          navigation.push("Results", {
+            result: result,
+          });
+        } else {
+          console.error(
+            "Something went wrong sending the data to the dashboard"
+          );
+        }
+      })
+      .catch((err) => {
+        console.log("Error sending data to elasticsearch ", err);
+      });
+  };
   return (
     <View style={styles.container}>
-      <Text>Please run a scan</Text>
-      <Button onPress={onScan} title='Scan' />
+      <Text style={styles.title}>Please run a scan</Text>
+      <Button onPress={onScan} title="Scan" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 16,
   },
 });
