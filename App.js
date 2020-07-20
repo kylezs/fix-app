006123d00b1dade910ca85f30bb039881c0c2c67
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-07-20 16:38:19 +1000

    Auth working

--- App.js
+++ App.js
@@ -3,7 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 
-import { Authenticator, SignIn } from "aws-amplify-react-native";
+import {
+  Authenticator,
+  SignIn,
+  SignUp,
+  RequireNewPassword,
+  VerifyContact,
+  ForgotPassword,
+  Greetings,
+} from "aws-amplify-react-native";
+import { Auth } from "aws-amplify";
 import { View, Text, Button } from "react-native";
 
 import {
@@ -15,7 +24,6 @@ import {
 import ResultsView from "./src/ResultsView";
 import ScanView from "./src/ScanView";
 import SettingsView from "./src/SettingsView";
-import { SignUp } from "aws-amplify-react-native/dist/Auth";
 
 // Contain the scan screen and results screen
 const Stack = createStackNavigator();
@@ -31,44 +39,65 @@ const Home = () => {
   );
 };
 
-function App() {
+const AppComponents = ({ authState }) => {
+  _validAuthStates = ["signedIn"];
+  console.log("Here's the authState in App components: " + authState);
   return (
-    <CustomAuthenticator>
+    <>
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name={HOME_SCREEN} component={Home} />
           <Tab.Screen name={SETTINGS_SCREEN} component={SettingsView} />
         </Tab.Navigator>
       </NavigationContainer>
-    </CustomAuthenticator>
+      <Greetings />
+    </>
   );
-}
+};
+
+const App = () => {
+  const user = Auth.currentAuthenticatedUser()
+    .then((user) => {
+      return user;
+    })
+    .catch((err) => console.log(err));
+  if (user) {
+    return <AppComponents />;
+  } else {
+    return <CustomAuthenticator />;
+  }
+};
 
 class MySignUp extends SignUp {
   constructor(props) {
     super(props);
-    this._validAuthStates = ["signUp", "signedOut", "signedUp"];
+    this._validAuthStates = ["signUp"];
   }
 
   render() {
+    console.log("My Sign Up");
     console.log(this.props);
     const { authState } = this.props;
-    if (authState === "signUp") {
-      return (
-        <View>
-          <Text>You cannot use this app unless you are cool</Text>
-          <Button title="Sign In" onPress={() => this.changeState("signIn")} />
-        </View>
-      );
-    }
-    return null;
+    return (
+      <View>
+        <Text>You cannot use this app unless you are cool</Text>
+        <Button title="Sign In" onPress={() => this.changeState("signIn")} />
+      </View>
+    );
   }
 }
 
 const CustomAuthenticator = () => {
   return (
-    <Authenticator usernameAttributes="email" hideDefault={true}>
+    <Authenticator
+      usernameAttributes="email"
+      hideDefault={true}
+      authState="signIn"
+    >
       <SignIn />
+      <ForgotPassword />
+      <RequireNewPassword />
+      <VerifyContact />
       <MySignUp override={"SignUp"} />
     </Authenticator>
   );
